0fd44697ed7b461014eea54db1085e5fef9afdd4
NomadWrld <moradamalco@gmail.com>
2023-12-05 09:41:22 -0800

    check

--- _printf.c
+++ _printf.c
@@ -1,75 +1,5 @@
-#include <limits.h>
-#include <stdio.h>
 #include "main.h"
-
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-        int len;
-        int len2;
-        unsigned int ui;
-        void *addr;
-
-        len = _printf("Let's try to printf a simple sentence.\n");
-        len2 = printf("Let's try to printf a simple sentence.\n");
-        ui = (unsigned int)INT_MAX + 1024;
-        addr = (void *)0x7ffe637541f0;
-        _printf("Length:[%d, %i]\n", len, len);
-        printf("Length:[%d, %i]\n", len2, len2);
-        _printf("Negative:[%d]\n", -762534);
-        printf("Negative:[%d]\n", -762534);
-        _printf("Unsigned:[%u]\n", ui);
-        printf("Unsigned:[%u]\n", ui);
-        _printf("Unsigned octal:[%o]\n", ui);
-        printf("Unsigned octal:[%o]\n", ui);
-        _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-        printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-        _printf("Character:[%c]\n", 'H');
-        printf("Character:[%c]\n", 'H');
-        _printf("String:[%s]\n", "I am a string !");
-        printf("String:[%s]\n", "I am a string !");
-        _printf("Address:[%p]\n", addr);
-        printf("Address:[%p]\n", addr);
-        len = _printf("Percent:[%%]\n");
-        len2 = printf("Percent:[%%]\n");
-        _printf("Len:[%d]\n", len);
-        printf("Len:[%d]\n", len2);
-        return (0);
-}
-#include "main.h"
-
-/**
- * _putchar - Writes a character to the standard output (stdout)
- * @c: The character to be written.
- * Return: The number of characters written.
- */
-int _putchar(char c)
-{
-        return (write(1, &c, 1));
-}
-
-/**
- * _puts - Writes a string to the standard output (stdout)
- * @str: The string to be written.
- * Return: The number of characters written.
- */
-int _puts(char *str)
-{
-        int count = 0;
-        char *ptr = str;
-
-        while (ptr && *ptr)
-        {
-                count += _putchar(*ptr);
-                ptr++;
-        }
-
-        return (count);
-}
+#include <stdarg.h>
 
 /**
  * _itoa - Converts an integer to a string.
@@ -79,32 +9,70 @@ int _puts(char *str)
  */
 int _itoa(int n, char *str)
 {
-        int i = 0, temp = 0;
+	int i = 0, temp = 0, is_neg = 0, j = 0;
 
-        if (n == 0)
-                str[i++] = '0';
-        else if (n < 0)
-        {
-                str[i++] = '-';
-                n = -n;
-        }
 
-        temp = n;
-        while (temp != 0)
-        {
-                temp /= 10;
-                i++;
-        }
+	if (n == 0)
+	{
+		str[i++] = '0';
+	}
+	else if (n < 0)
+	{
+		is_neg = 1;
+		n = -n;
+	}
 
-        str[i] = '\0';
+	temp = n;
+	while (temp != 0)
+	{
+		str[i++] = '0' + (temp % 10);
+		temp /= 10;
+	}
 
-        while (n != 0)
-        {
-                str[--i] = '0' + (n % 10);
-                n /= 10;
-        }
+	if (is_neg)
+		str[i++] = '-';
 
-        return (i);
+	for (j = 0; j < i / 2; j++)
+	{
+		char temp_char = str[j];
+
+		str[j] = str[i - j - 1];
+		str[i - j - 1] = temp_char;
+	}
+
+	str[i] = '\0';
+
+	return (i);
+}
+
+
+/**
+ * _putchar - Writes a character to the standard output (stdout)
+ * @c: The character to be written.
+ * Return: The number of characters written.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _puts - Writes a string to the standard output (stdout)
+ * @str: The string to be written.
+ * Return: The number of characters written.
+ */
+int _puts(char *str)
+{
+	int count = 0;
+	char *ptr = str;
+
+	while (ptr && *ptr)
+	{
+		count += _putchar(*ptr);
+		ptr++;
+	}
+
+	return (count);
 }
 
 /**
@@ -115,56 +83,44 @@ int _itoa(int n, char *str)
  */
 int _printf(const char *format, ...)
 {
-        char *str, buf[11];
-        int i = 0, len = 0, n = 0, count = 0;
-        va_list args;
+	char *str, buf[11];
+	int i = 0, n = 0, count = 0;
+	va_list args;
 
-        va_start(args, format);
-        while (format && format[i])
-        {
-                if (format[i] == '%')
-                {
-                        i++;
-                        switch (format[i])
-                        {
-                                case 'c':
-                                        count += _putchar(va_arg(args, int));
-                                        break;
-                                case 's':
-                                        str = va_arg(args, char *);
-                                        count += _puts(str);
-                                        break;
-                                case '%':
-                                        count += _putchar('%');
-                                        break;
-                                case 'd':
-                                case 'i':
-                                                n = va_arg(args, int);
-                                                len = _itoa(n, buf);
-                                                count += _puts(buf + len);
-                                                break;
-                                default:
-                                        count += _putchar('%');
-                                        count += _putchar(format[i]);
-                                        break;
-                        }
-                }
-                else
-                        count += _putchar(format[i]);
-                i++;
-        }
-        va_end(args);
-        return (count);
+	va_start(args, format);
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					count += _putchar(va_arg(args, int));
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					count += _puts(str);
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+				case 'd':
+				case 'i':
+					n = va_arg(args, int);
+					_itoa(n, buf);
+					count += _puts(buf);
+					break;
+				default:
+					count += _putchar('%');
+					count += _putchar(format[i]);
+					break;
+			}
+		}
+		else
+			count += _putchar(format[i]);
+		i++;
+	}
+	va_end(args);
+	return (count);
 }
-#ifndef PRINTF_H
-#define PRINTF_H
-
-#include <stdarg.h>
-#include <unistd.h>
-
-int _putchar(char c);
-int _puts(char *str);
-int _itoa(int n, char *str);
-int _printf(const char *format, ...);
-
-#endif /* PRINTF_H */
