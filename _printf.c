ea85b8f0fb2604aa7c3d1b618d6b1dd4a075ed76
NomadWrld <moradamalco@gmail.com>
2023-12-05 09:30:56 -0800

    test

--- _printf.c
+++ _printf.c
@@ -1,50 +1,47 @@
+#include <limits.h>
+#include <stdio.h>
 #include "main.h"
-#include <stdarg.h>
 
 /**
- * _itoa - Converts an integer to a string.
- * @n: The integer to be converted.
- * @str: The buffer to store the resulting string.
- * Return: The length of the string.
+ * main - Entry point
+ *
+ * Return: Always 0
  */
-int _itoa(int n, char *str)
+int main(void)
 {
-	int i = 0, temp = 0, is_neg = 0, j = 0;
+	int len;
+	int len2;
+	unsigned int ui;
+	void *addr;
 
-
-	if (n == 0)
-	{
-		str[i++] = '0';
-	}
-	else if (n < 0)
-	{
-		is_neg = 1;
-		n = -n;
-	}
-
-	temp = n;
-	while (temp != 0)
-	{
-		str[i++] = '0' + (temp % 10);
-		temp /= 10;
-	}
-
-	if (is_neg)
-		str[i++] = '-';
-
-	for (j = 0; j < i / 2; j++)
-	{
-		char temp_char = str[j];
-
-		str[j] = str[i - j - 1];
-		str[i - j - 1] = temp_char;
-	}
-
-	str[i] = '\0';
-
-	return (i);
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	ui = (unsigned int)INT_MAX + 1024;
+	addr = (void *)0x7ffe637541f0;
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	_printf("Unsigned:[%u]\n", ui);
+	printf("Unsigned:[%u]\n", ui);
+	_printf("Unsigned octal:[%o]\n", ui);
+	printf("Unsigned octal:[%o]\n", ui);
+	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	_printf("Address:[%p]\n", addr);
+	printf("Address:[%p]\n", addr);
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
+	return (0);
 }
 
+#include "main.h"
 
 /**
  * _putchar - Writes a character to the standard output (stdout)
@@ -53,7 +50,7 @@ int _itoa(int n, char *str)
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	return write(1, &c, 1);
 }
 
 /**
@@ -63,16 +60,56 @@ int _putchar(char c)
  */
 int _puts(char *str)
 {
+	if (str == NULL)
+		return 0;
+
 	int count = 0;
 	char *ptr = str;
 
-	while (ptr && *ptr)
+	while (*ptr)
 	{
 		count += _putchar(*ptr);
 		ptr++;
 	}
 
-	return (count);
+	return count;
+}
+
+/**
+ * _itoa - Converts an integer to a string.
+ * @n: The integer to be converted.
+ * @str: The buffer to store the resulting string.
+ * Return: The length of the string.
+ */
+int _itoa(int n, char *str)
+{
+	int i = 0, temp = 0;
+	// Buffer size check
+	if (n == 0)
+		str[i++] = '0';
+	else if (n < 0)
+	{
+		str[i++] = '-';
+		n = -n;
+	}
+
+	temp = n;
+	while (temp != 0)
+	{
+		temp /= 10;
+		i++;
+	}
+
+	str[i] = '\0';
+
+	// Buffer size check
+	while (n != 0)
+	{
+		str[--i] = '0' + (n % 10);
+		n /= 10;
+	}
+
+	return i;
 }
 
 /**
@@ -83,8 +120,8 @@ int _puts(char *str)
  */
 int _printf(const char *format, ...)
 {
-	char *str, buf[11];
-	int i = 0, n = 0, count = 0;
+	char buf[11];
+	int i = 0, len = 0, n = 0, count = 0;
 	va_list args;
 
 	va_start(args, format);
@@ -99,8 +136,7 @@ int _printf(const char *format, ...)
 					count += _putchar(va_arg(args, int));
 					break;
 				case 's':
-					str = va_arg(args, char *);
-					count += _puts(str);
+					count += _puts(va_arg(args, char *));
 					break;
 				case '%':
 					count += _putchar('%');
@@ -108,8 +144,8 @@ int _printf(const char *format, ...)
 				case 'd':
 				case 'i':
 					n = va_arg(args, int);
-					_itoa(n, buf);
-					count += _puts(buf);
+					len = _itoa(n, buf);
+					count += _puts(buf + len);
 					break;
 				default:
 					count += _putchar('%');
@@ -122,5 +158,19 @@ int _printf(const char *format, ...)
 		i++;
 	}
 	va_end(args);
-	return (count);
+	return count;
 }
+
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int _puts(char *str);
+int _itoa(int n, char *str);
+int _printf(const char *format, ...);
+
+#endif /* PRINTF_H */
+
