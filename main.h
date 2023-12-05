5793ce1e1e4d08b65a66ddbc870853206bc16720
NomadWrld <moradamalco@gmail.com>
2023-12-05 11:09:51 -0800

    hope3.0

--- main.h
+++ main.h
@@ -4,9 +4,10 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+char *_strncpy(char *dest, const char *src, size_t n);
 int _putchar(char c);
 int _puts(char *str);
-int _itoa(int n, char *str);
+int _itoa(int n, char *str, int buf_size);
 int _printf(const char *format, ...);
 
 #endif /* PRINTF_H */
